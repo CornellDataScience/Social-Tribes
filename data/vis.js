0a4e1246d2432e9be380c595f4fe153780ca0e07
kevinngonyc <kevinngonyc@gmail.com>
2018-04-25 15:02:55 -0400

    d3 stuffs

--- data/vis.js
+++ data/vis.js
@@ -2,10 +2,17 @@ var margin = {
 	top: 20,
 	right: 20,
 	bottom: 50,
-	left: 60
+	left: 20
 	},
 	width = 600 - margin.left - margin.right,
 	height = 500 - margin.top - margin.bottom;
+
+var algo = 0;
+
+function algo_change(x){
+	document.getElementByID('algo');
+	algo = x;
+}
 
 var canvas = d3.select("body").select("svg")
 	.attr("width", width + margin.left + margin.right)
@@ -46,12 +53,8 @@ d3.csv('pca.csv', function (data){
 			return y(d.Comp2);
 		})
 		.attr('fill', function (d) {
-			if(d.Followers > 1000000)
-				return color(1);
-			else if(d.Followers > 50000)
-			 	return color(2);
-			else
-				return color(3);
+			var cluster_algos = [d.Gaussian, d.Spectral, d.Agglomerative, d.KMeans];
+			return color(cluster_algos[algo]);
 		});
 
 	// var xAxis = d3.axisBottom(x);
@@ -91,132 +94,132 @@ d3.csv('pca.csv', function (data){
 	// 	});
 
 	// K means
-//
-// 	var lines, circles, centroids;
-// 	var points = [];
-//
-// 	for (var i = 0; i < sl.length; i++) {
-// 		points.push({
-// 			cluster: -1,
-// 			x: sl[i],
-// 			y: sw[i]
-// 		});
-// 	};
-//
-// 	lines = canvas.selectAll('line').data(points)
-// 		.enter().append('line')
-// 		.attr('x1', function (d) {
-// 			return x(d.x);
-// 		})
-// 		.attr('y1', function (d) {
-// 			return y(d.y);
-// 		})
-// 		.attr('x2', function (d) {
-// 			return x(d.x);
-// 		})
-// 		.attr('y2', function (d) {
-// 			return y(d.y);
-// 		})
-// 		.attr('stroke', 'grey')
-// 		.attr('stroke-width', '1px')
-// 		.attr('opacity', 0.7);
-//
-// 	centroids = new Array(3);
-// 	for (var i = 0; i < centroids.length; i++) {
-// 		var centroid_seed = Math.round(Math.random() * points.length);
-// 		console.log(points[centroid_seed]);
-// 		centroids[i] = {
-// 			x: points[centroid_seed].x,
-// 			y: points[centroid_seed].y
-// 		}
-// 	}
-//
-// 	var centroidCircles = canvas.selectAll('.centroid').data(centroids)
-// 		.enter().append('circle')
-// 		.attr('class', 'centroid')
-// 		.attr('r', 5)
-// 		.attr('fill', '#333333')
-// 		.attr('cx', function (d) {
-// 			return x(d.x);
-// 		})
-// 		.attr('cy', function (d) {
-// 			return y(d.y);
-// 		});
-//
-// 	function nearest(point, candidates) {
-// 		var nearest;
-// 		var shortestDistance = Number.MAX_VALUE;
-// 		for (var i = 0; i<candidates.length; i++) {
-// 			var c = candidates[i];
-// 			var distance = Math.sqrt(
-// 				(c.x - point.x) * (c.x - point.x) +
-// 				(c.y - point.y) * (c.y - point.y)
-// 			);
-//
-// 			if (distance < shortestDistance) {
-// 				shortestDistance = distance;
-// 				nearest = i;
-// 			}
-// 		}
-// 		return nearest;
-// 	}
-//
-// 	function moveMeans() {
-// 		centroids.forEach(function (centroid, i) {
-// 			var assignedPoints = points.filter(function (point) {
-// 				return point.cluster == i;
-// 			});
-// 			centroid.x = d3.mean(assignedPoints, function (d) {
-// 				return d.x;
-// 			});
-// 			centroid.y = d3.mean(assignedPoints, function (d) {
-// 				return d.y;
-// 			});
-// 		});
-//
-// 		centroidCircles.transition().duration(1000)
-// 			.attr('cx', function (d) {
-// 				return x(d.x);
-// 			})
-// 			.attr('cy', function (d) {
-// 				return y(d.y);
-// 			});
-//
-// 		lines.transition().duration(1000)
-// 			.attr('x2', function (point) {
-// 				return x(centroids[point.cluster].x);
-// 			})
-// 			.attr('y2', function (point) {
-// 				return y(centroids[point.cluster].y);
-// 			});
-// 	}
-//
-// 	function findClosest() {
-// 		var ct = 0;
-// 		points.forEach(function (point) {
-// 			var newCluster = nearest(point, centroids);
-// 			point.cluster = newCluster;
-// 		});
-//
-// 		lines.transition().duration(1000)
-// 			.attr("x2", function (point) {
-// 				return x(centroids[point.cluster].x);
-// 		})
-// 			.attr("y2", function (point) {
-// 				return y(centroids[point.cluster].y);
-// 		});
-// 	}
-//
-// 	function cluster() {
-// 		findClosest();
-// 		moveMeans();
-// 		setInterval(function () {
-// 			findClosest();
-// 			moveMeans();
-// 		}
-// 		, 1000);
-//
-// 	}
-//
-// 	d3.select('#cluster').on('click', cluster);
+
+	var lines, circles, centroids;
+	var points = [];
+
+	for (var i = 0; i < sl.length; i++) {
+		points.push({
+			cluster: -1,
+			x: sl[i],
+			y: sw[i]
+		});
+	};
+
+	lines = canvas.selectAll('line').data(points)
+		.enter().append('line')
+		.attr('x1', function (d) {
+			return x(d.x);
+		})
+		.attr('y1', function (d) {
+			return y(d.y);
+		})
+		.attr('x2', function (d) {
+			return x(d.x);
+		})
+		.attr('y2', function (d) {
+			return y(d.y);
+		})
+		.attr('stroke', 'grey')
+		.attr('stroke-width', '1px')
+		.attr('opacity', 0.7);
+
+	centroids = new Array(3);
+	for (var i = 0; i < centroids.length; i++) {
+		var centroid_seed = Math.round(Math.random() * points.length);
+		console.log(points[centroid_seed]);
+		centroids[i] = {
+			x: points[centroid_seed].x,
+			y: points[centroid_seed].y
+		}
+	}
+
+	var centroidCircles = canvas.selectAll('.centroid').data(centroids)
+		.enter().append('circle')
+		.attr('class', 'centroid')
+		.attr('r', 5)
+		.attr('fill', '#333333')
+		.attr('cx', function (d) {
+			return x(d.x);
+		})
+		.attr('cy', function (d) {
+			return y(d.y);
+		});
+
+	function nearest(point, candidates) {
+		var nearest;
+		var shortestDistance = Number.MAX_VALUE;
+		for (var i = 0; i<candidates.length; i++) {
+			var c = candidates[i];
+			var distance = Math.sqrt(
+				(c.x - point.x) * (c.x - point.x) +
+				(c.y - point.y) * (c.y - point.y)
+			);
+
+			if (distance < shortestDistance) {
+				shortestDistance = distance;
+				nearest = i;
+			}
+		}
+		return nearest;
+	}
+
+	function moveMeans() {
+		centroids.forEach(function (centroid, i) {
+			var assignedPoints = points.filter(function (point) {
+				return point.cluster == i;
+			});
+			centroid.x = d3.mean(assignedPoints, function (d) {
+				return d.x;
+			});
+			centroid.y = d3.mean(assignedPoints, function (d) {
+				return d.y;
+			});
+		});
+
+		centroidCircles.transition().duration(1000)
+			.attr('cx', function (d) {
+				return x(d.x);
+			})
+			.attr('cy', function (d) {
+				return y(d.y);
+			});
+
+		lines.transition().duration(1000)
+			.attr('x2', function (point) {
+				return x(centroids[point.cluster].x);
+			})
+			.attr('y2', function (point) {
+				return y(centroids[point.cluster].y);
+			});
+	}
+
+	function findClosest() {
+		var ct = 0;
+		points.forEach(function (point) {
+			var newCluster = nearest(point, centroids);
+			point.cluster = newCluster;
+		});
+
+		lines.transition().duration(1000)
+			.attr("x2", function (point) {
+				return x(centroids[point.cluster].x);
+		})
+			.attr("y2", function (point) {
+				return y(centroids[point.cluster].y);
+		});
+	}
+
+	function cluster() {
+		findClosest();
+		moveMeans();
+		setInterval(function () {
+			findClosest();
+			moveMeans();
+		}
+		, 1000);
+
+	}
+
+	d3.select('#cluster').on('click', cluster);
 });
