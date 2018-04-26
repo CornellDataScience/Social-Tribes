05e98c33bae47de284b56373ffb52cad9ab440f2
kevinngonyc <kevinngonyc@gmail.com>
2018-04-26 17:37:40 -0400

    BEAUTIFUL!

--- data/vis.js
+++ data/vis.js
@@ -7,6 +7,25 @@ var margin = {
 	width = 800 - margin.left - margin.right,
 	height = 600 - margin.top - margin.bottom;
 
+d3.selection.prototype.moveToFront = function(){
+		return this.each(function(){
+			this.parentNode.appendChild(this);
+		});
+};
+
+var prominents = [[['Dave Weigel', 'Mark Halperin', 'Ben Smith'],
+  ['Rachel Maddow MSNBC', 'Anderson Cooper', 'Paul Krugman'],
+  ['Jonathan Martin', 'Zeke Miller', 'Mike Allen']],
+ [['Mike Allen', 'andrew kaczynski\xf0\x9f\xa4\x94', 'Ben Smith'],
+  ['Sean Hannity', 'Anderson Cooper', 'Arianna Huffington'],
+  ['Nate Silver', 'Paul Krugman', 'Rachel Maddow MSNBC']],
+ [['Mike Allen', 'andrew kaczynski\xf0\x9f\xa4\x94', 'Ben Smith'],
+  ['Anderson Cooper', 'Paul Krugman', 'Rachel Maddow MSNBC'],
+  ['Terry Moran', 'Robert Costa', 'Dave Weigel']],
+ [['Rachel Maddow MSNBC', 'Anderson Cooper', 'Paul Krugman'],
+  ['Jonathan Martin', 'Zeke Miller', 'Mike Allen'],
+  ['Dave Weigel', 'Mark Halperin', 'Ben Smith']]]
+
 var canvas = d3.select("body").select("svg")
 	.attr("width", width + margin.left + margin.right)
 	.attr('height', height + margin.top + margin.bottom)
@@ -30,9 +49,8 @@ d3.csv('pca.csv', function (data){
 		.range([height,0])
 		.domain([-1.5, 1.5])
 
-	var color = d3.scaleOrdinal(['#4abdac','#fc4a1a', '#f7b733']);
-	console.log(d3.schemeCategory10);
-
+	var hexColors = ['#4abdac','#fc4a1a', '#f7b733']
+	var color = d3.scaleOrdinal(hexColors);
 
 	var circle = canvas.selectAll('.dot')
 		.data(data)
@@ -66,7 +84,16 @@ d3.csv('pca.csv', function (data){
                 })
                 .style('opacity', '0.9')
                 .style('display', 'inline-block')
-            div.html("<span style='font-size:26'>" + d.Names + "</span>  <span style='color:	#484848; font-style:italic'>" + d.Followers.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + " followers </span>" + d.Descriptions);
+						var cluster_algos = [d.Gaussian, d.Spectral, d.Agglomerative, d.KMeans];
+            div.html(
+							"<span style='font-size:26'>" + d.Names + "</span> <br>" +
+							"<span style='color:	#484848; font-style:italic'>" + d.Followers.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",").substring(0, d.Followers.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",").length - 2) + " followers </span><br><br>" +
+							d.Descriptions + "<br><br>" +
+							"_____________________________________________<br><br>" +
+							`<div style='font-size:17.5; line-height:40px'><center><i>Prominent Journalists Within Cluster</i></center></div>` +
+							`<span style='font-size:16; color:#202020;line-height:30px'><center>${prominents[algo][cluster_algos[algo]][0]}<br>${prominents[algo][cluster_algos[algo]][1]}<br>${prominents[algo][cluster_algos[algo]][2]}<br><br><span style='font-size:80; color:${hexColors[cluster_algos[algo]]}'>&#9673</span></center></span>`
+						);
+
         })
         .on("mouseout", function (d) {
             div.transition().duration(500)
@@ -176,11 +203,17 @@ d3.csv('pca.csv', function (data){
 	}
 
 	function cluster(lines, centroids, centroidCircles, points) {
+
+		var timesRun = 0;
 		findClosest(lines, centroids, points);
 		moveMeans(lines, centroids, centroidCircles, points);
-		setInterval(function () {
+		var interval = setInterval(function () {
+			timesRun += 1;
 			findClosest(lines, centroids, points);
 			moveMeans(lines, centroids, centroidCircles, points);
+			if(timesRun === 20){
+	        clearInterval(interval);
+	    }
 		}
 		, 1000);
 
@@ -198,9 +231,13 @@ d3.csv('pca.csv', function (data){
 		});
 	}
 
-	function gaussian(){algo_change(0)}
-	function spectral(){algo_change(1)}
-	function agglomerative(){algo_change(2)}
+	function clearKMeans(){
+		d3.selectAll('line').remove();
+		d3.selectAll('.centroid').remove();
+	}
+	function gaussian(){clearKMeans();algo_change(0)}
+	function spectral(){clearKMeans();algo_change(1)}
+	function agglomerative(){clearKMeans();algo_change(2)}
 	function kmeans(){
 			var lines, circles, centroids;
 			var points = [];
@@ -256,7 +293,113 @@ d3.csv('pca.csv', function (data){
 		cluster(lines, centroids, centroidCircles, points);
 	}
 
+	function handleClick(){
+      var name = document.getElementById("myVal").value
+			for(var j in data){
+				if(data[j]['Names'].includes(name)){
+					console.log(data[j]['Names'])
+					var div = d3.select("body").select("#innertext")
+				        .attr("class", "tooltip")
+				        .style("opacity", 0);
 
+          div.transition().duration(500)
+              .style('left', d3.event.pageX - 20 + "px")
+              .style('top', function (d) {
+                  return d3.event.pageY > height ? d3.event.pageY - 30 + 'px' : d3.event.pageY + 20 + 'px';
+              })
+              .style('opacity', '0.9')
+              .style('display', 'inline-block')
+					var cluster_algos = [data[j]['Gaussian'], data[j]['Spectral'], data[j]['Agglomerative'], data[j]['KMeans']];
+					div.html(
+						"<span style='font-size:26'>" + data[j]['Names'] + "</span> <br>" +
+						"<span style='color:	#484848; font-style:italic'>" + data[j]['Followers'].toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",").substring(0, data[j]['Followers'].toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",").length - 2) + " followers </span><br><br>" +
+						data[j]['Descriptions'] + "<br><br>" +
+						"_____________________________________________<br><br>" +
+						`<div style='font-size:17.5; line-height:40px'><center><i>Prominent Journalists Within Cluster</i></center></div>` +
+						`<span style='font-size:16; color:#202020;line-height:30px'><center>${prominents[algo][cluster_algos[algo]][0]}<br>${prominents[algo][cluster_algos[algo]][1]}<br>${prominents[algo][cluster_algos[algo]][2]}<br><br><span style='font-size:80; color:${hexColors[cluster_algos[algo]]}'>&#9673</span></center></span>`
+					);
+					canvas.selectAll('circle').filter(function (d) {
+						return d.Names == data[j]['Names'];}).moveToFront();
+
+
+
+
+					circle.transition().duration(1000)
+						.filter(function (d) {
+							return d.Names == data[j]['Names'];})
+						.attr('fill', '#5AD75A')
+						.attr("r", function (d) {
+							return 8 * Math.pow(d.Followers,0.1);
+						})
+            .transition()
+            .duration(800)
+            .attr("r", function (d) {
+							return 8 * Math.pow(d.Followers,0.3) / 50;
+						})
+						.transition()
+            .duration(800)
+						.attr("r", function (d) {
+							return 8 * Math.pow(d.Followers,0.1);
+						})
+						.transition()
+            .duration(800)
+            .attr("r", function (d) {
+							return 8 * Math.pow(d.Followers,0.3) / 50;
+						})
+						.transition()
+            .duration(800)
+						.attr("r", function (d) {
+							return 8 * Math.pow(d.Followers,0.1);
+						})
+            .transition()
+            .duration(200)
+            .attr("r", function (d) {
+							return 8 * Math.pow(d.Followers,0.3) / 50;
+						})
+						.attr('fill', function (d) {
+							var cluster_algos = [d.Gaussian, d.Spectral, d.Agglomerative, d.KMeans];
+							return color(cluster_algos[algo]);
+						})
+
+						;
+				}
+			}
+  }
+
+	// canvas.selectAll("circle")
+	// .on("click", function(d) {
+	// 	d.pulse = !d.pulse;
+	// 	if (d.pulse) {
+	// 		var selected_circles = d3.select(this);
+	// 		console.log(selected_circles);
+	// 		pulsate(selected_circles);
+  //
+	// 	}
+	// });
+  //
+	// function pulsate(selection) {
+  //   recursive_transitions();
+  //
+  //   function recursive_transitions() {
+	// 		console.log("HIT")
+  //     if (selection.data()[0].pulse) {
+  //       selection.transition()
+  //           .duration(400)
+  //           .attr("r", 8)
+  //           .transition()
+  //           .duration(800)
+  //           .attr("r", 12)
+  //           .each("end", recursive_transitions);
+  //     } else {
+  //       // transition back to normal
+  //       selection.transition()
+  //           .duration(200)
+  //           .attr("r", 8)
+  //     }
+  //   }
+  // }
+
+	d3.select('#select').on('click', handleClick);
 	d3.select('#gaussian').on('click', gaussian);
 	d3.select('#spectral').on('click', spectral);
 	d3.select('#agglomerative').on('click', agglomerative);
